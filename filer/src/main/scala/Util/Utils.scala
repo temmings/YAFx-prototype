ecad3748faccc6ecf646f99a49b0bfc660ce8c17
Tetsuya HAMAMOTO <temmings@gmail.com>
2017-01-19 03:11:13 +0900

    InputStream.available がサポートされていない場合にバイナリ判定できるようにする。

--- filer/src/main/scala/Util/Utils.scala
+++ filer/src/main/scala/Util/Utils.scala
@@ -5,34 +5,17 @@ import java.io.InputStream
 import org.mozilla.universalchardet.UniversalDetector
 
 object Utils {
-  /**
-    *  Guess whether given file is binary. Just checks for anything under 0x09.
-    *  http://stackoverflow.com/questions/620993/determining-binary-text-file-type-in-java
-    */
   def isBinary(in: InputStream): Boolean = {
     // TODO: detect UTF-16
     // TODO: detect BOM
-    val maxSize: Int = 1024
-    val size = if (in.available < maxSize) in.available else maxSize
-    val data = new Array[Byte](size)
-    in.read(data)
+    val size: Int = 1024
+    val buf = new Array[Byte](size)
+    val length = in.read(buf)
+    val data = buf.take(length)
 
-    var ascii = 0
-    var other = 0
-
-    for (i <- data.indices) {
-      val b = data(i)
-      //println(f"$i: $b : $b%02X")
-      if (0x00 <= b && b < 0x09) return true
-
-      if (b == 0x09 || b == 0x0A || b == 0x0C || b == 0x0D) ascii += 1
-      else if (0x20 <= b && b <= 0x7E) ascii += 1
-      else other += 1
-    }
-
-    if (other == 0) return false
-
-    100 * other / (ascii + other) > 95
+    if (data.exists((0x00 to 0x08).contains(_))) return true
+    val asciiBytes = (0x09 to 0x0A) ++ (0x0C to 0x0D) ++ (0x20 to 0x7E)
+    (100 * data.count(asciiBytes.contains)) / length <= 5
   }
 
   def detectCharset(in: InputStream): Option[String] = {
